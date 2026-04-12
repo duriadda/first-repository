69227ff4cc1a03f711e2be29b3b4ae1bcbcb42bc
duriadda <133933490+duriadda@users.noreply.github.com>
2026-04-12 16:39:44 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+    {
+        printf("%d", 10000 + a * 1000);
+    }
+    else if (a == b || a == c)
+    {
+        printf("%d", 1000 + a * 100);
+    }
+    else if (b == c)
+    {
+        printf("%d", 1000 + b * 100);
+    }
+    else
+    {
+        if(a > b && a > c)
+        {
+            printf("%d", a * 100);
+        }
+        else if (b > a && b > c)
+        {
+            printf("%d", b * 100);
+        }
+        else
+        {
+            printf("%d", c * 100);
+        }
+    }
+
+}
+
+/*int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+printf("%d", max * 100);*/
